d35a64f53345210b4a473a65ec9ef5c9c23ea287
mwegenstein <mwegenstein@gmail.com>
2013-06-09 20:44:23 -0700

    Removed Camera Slider. Introduced Royal Slider.

--- app/js/app.js
+++ app/js/app.js
@@ -9,20 +9,6 @@ App.common = (function($){
 App.collection = (function($) {
   var self = {};
 
-  var loadCollectionSlideshow = function() {
-  	$('#water-casting-slideshow').camera({
-    	fx: 'simpleFade',
-			height: '70%',
-			loader: 'none',
-      slideOn:'prev',
-			pagination: false,
-			thumbnails: true,
-      playPause: false,
-      time: 4000,
-      transPeriod: 1000
-		});
-  };
-
   var collectionHover = function() {
     var categories = $('.collection').find('.category');
 
@@ -33,26 +19,13 @@ App.collection = (function($) {
     });
   };
 
-  var setSectionsToFullScreen = function() {
-    $('.full-screen').css({'min-height': self.height, 'min-width': self.width});
-  }
-  var resizeFullScreens = function() {
-    $(window).resize(function() {
-      self.width = $(this).width();
-      self.height = $(this).height();
-      setSectionsToFullScreen();
-    });
-  }
-
   self.width = $(window).width();
   self.height = $(window).width();
 
   self.init = function() {
-    loadCollectionSlideshow();
     collectionHover();
-    // setSectionsToFullScreen();
-    // resizeFullScreens();
   }
 
   return self;
 })(jQuery);
+
