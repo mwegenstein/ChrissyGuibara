181c87c99a76e27791ca916d8681a057d90d9c6b
mwegenstein <mwegenstein@gmail.com>
2013-06-11 10:10:31 -0700

    Home + About Page
    added new edited photos for both home and about, updated styling on
    about page

--- app/js/app.js
+++ app/js/app.js
@@ -6,7 +6,8 @@ App.common = (function($){
   return self;
 })(jQuery);
 
-App.collection = (function($) {
+// Collections Module
+App.collections = (function($) {
   var self = {};
 
   var collectionHover = function() {
@@ -16,14 +17,48 @@ App.collection = (function($) {
       categories.not(this).addClass('opaque');
     }, function(){
       categories.removeClass('opaque');
-    });
-  };
+    });};
 
   self.width = $(window).width();
   self.height = $(window).width();
 
   self.init = function() {
     collectionHover();
+  }
+
+  return self;
+})(jQuery);
+
+// Collection Module
+App.collection = (function($) {
+  var self = {};
+
+  var createSlider = function() {
+    $(".royalSlider").royalSlider({
+        fullscreen: {
+          enabled: true,
+          nativeFS: true
+        },
+        controlNavigation: 'thumbnails',
+        thumbs: {
+          orientation: 'vertical',
+          paddingBottom: 4,
+          appendSpan: true
+        },
+        transitionType:'fade',
+        autoScaleSlider: true, 
+        autoScaleSliderWidth: 960,     
+        autoScaleSliderHeight: 600,
+        loop: true,
+        arrowsNav: false,
+        keyboardNavEnabled: true,
+        globalCaption:true
+
+     });
+  };
+
+  self.init = function() {
+    createSlider();
   }
 
   return self;
